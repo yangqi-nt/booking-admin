c1a46b0dbb2784e4155bd1b1ee20c0321b16245e
yangqi <yangqiphp@163.com>
2021-05-17 11:42:57 +0800

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,6 +45,8 @@ return [
         'admin' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
         ],
         'local' => [
             'driver' => 'local',
